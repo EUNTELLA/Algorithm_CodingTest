df86fc2e4d50e40e0e3c8f0d7c4f7f467dbe2f87
EUNTELLA <yokohamay0612@gmail.com>
2025-08-02 15:06:05 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 136 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,51 +1,13 @@
 import sys
-input = sys.stdin.readline
+input= sys.stdin.readline
 
 n = int(input())
-
-arr= list(map(int,input().split()))
-
-result = []
-
-def binary_search(target):
-    
-    left = 0
-    right = len(result) -1
-    
-    while left <= right:
-        mid = (left + right)//2
-        
-        if result[mid] == target:
-            return mid
-        elif result[mid] <target:
-            left = mid + 1
-            
-        else:
-            right = mid-1
-    
-    
-    return left #가장 최근의 mid값이 left 로 이동
-
-
-for number in arr :
-    if len(result) ==0:
-        result.append(number)
-        continue
-
-    max_val = result[-1]
-
-
-
-    if number > max_val:
-        result.append(number)
-    else: #number < max_val
-        index = binary_search(number)
-    
-        result [index] = number
-
-print(len(result))
-    
-    
+arr = list(map(int, input().split()))
 
     
-    
+dp = [1] * n
+for i in range(1,n):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i],dp[j]+1)
+print(max(dp))
